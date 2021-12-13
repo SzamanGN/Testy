8ce05b624bb1f717bfe8a57e2604d416fc37999e
SzamanGN <tester@gmail.com>
2021-12-13 18:50:38 +0100

    Testy - zmienne

---
+++ Testy-Zmienne/src/zmienne/StartTestow.java
@@ -0,0 +1,10 @@
+package zmienne;
+
+public class StartTestow {
+
+	public static void main(String[] args) {
+		System.out.println("Start aplikacji Testy-Zmienne.");
+		new Testy();
+	}
+
+}
---
+++ Testy-Zmienne/src/zmienne/Testy.java
@@ -0,0 +1,14 @@
+package zmienne;
+
+public class Testy {
+
+	public Testy() {
+		pokaz("Start.");
+		
+		pokaz("Koniec.");
+	}
+
+	private void pokaz(String wpis) {
+		System.out.println("<Testy> " + wpis);
+	}
+}
